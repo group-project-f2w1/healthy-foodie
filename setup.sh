6df8ed18eb71b4456025e821ad6c5137cfda0b33
rey-h8 <rey-h8@gmail.com>
2020-10-28 18:16:37 +0700

    initial db migrations

---
+++ setup.sh
@@ -0,0 +1,4 @@
+npx sequelize model:create --name User --attributes name:string,email:string,password:string,avatarUrl:string,themeImageUrl:string
+
+npx sequelize model:create --name FavoriteRecipe --attributes UserId:integer,RecipeId:integer
+
